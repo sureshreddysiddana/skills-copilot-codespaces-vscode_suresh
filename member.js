3892d952a683e3559366416e44c1bd56449c6527
sureshreddysiddana <105904978+sureshreddysiddana@users.noreply.github.com>
2024-05-28 02:14:46 +0000

    Copilot Second Commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var skills = {
+    name: 'John Doe',
+    age: 30,
+    skills: ['HTML', 'CSS', 'JS'],
+    details: {
+      id: 1,
+      isMember: true
+    }
+  };
+  return skills;
+}
